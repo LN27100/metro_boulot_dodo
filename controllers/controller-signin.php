4c4fc408562e7267844d2e10c4294262ea677f29
LN27100 <heloceinlove@hotmail.fr>
2024-01-16 16:01:27 +0100

    test champs valides verts

--- controllers/controller-signin.php
+++ controllers/controller-signin.php
@@ -1,15 +1,16 @@
 <?php
+// Lier le fichier config
+require_once '../config.php';
+require_once '../models/Userprofil.php';
 
 $showform = true;
+$errors = array();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $errors = array();
 
-    // Récupération des données du formulaire en le rendant "safe" (enlever les caractères spéciaux etc)
-    
+    // Récupération des données du formulaire en les rendant "safe" (enlever les caractères spéciaux, etc.)
     $email = trim(htmlspecialchars($_POST['email']));
     $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
-
 
     // Contrôle de l'email
     if (empty($_POST["email"])) {
@@ -18,7 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errors["email"] = "Le format de l'adresse email n'est pas valide";
     }
 
-
     // Contrôle du mot de passe
     if (empty($_POST["mot_de_passe"])) {
         $errors["mot_de_passe"] = "Le champ Mot de passe ne peut pas être vide";
@@ -26,24 +26,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errors["mot_de_passe"] = "Le mot de passe doit contenir au moins 8 caractères";
     }
 
-    
-
-    // On s'assure qu'il n'y a pas d'erreur dans le formuaire
     if (empty($errors)) {
 
+        $userProfile = new Userprofil();
         $showform = false;
     }
-
-    // Donne toutes les propriétés du serveur
-    // var_dump($_SERVER)
 }
 
 include_once  '../views/view-signin.php';
-
-//lier le fichier config
-require_once '../config.php';
-require_once '../models/Userprofil.php';
-
-
-
 ?>
--- views/view-signin.php
+++ views/view-signin.php
@@ -13,53 +13,86 @@
 
 <body>
     <h1>Veuillez vous connecter</h1>
-    <?php
-    if ($showform) {
-    ?>
+    <div class="container">
+        <?php
+        if ($showform) {
+        ?>
+            <form class="row" method="POST" action="../controllers/controller-signin.php" novalidate>
 
-        <form class="row" method="POST" action="../controllers/controller-signin.php" novalidate>
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form2Example1">Email: </label>
-                <input type="email" class="form-control <?php if (isset($errors['email'])) echo 'is-invalid'; ?>" id="email" name="email" placeholder="adresse email" value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>" required>
-                <div class="invalid-feedback" id="emailValidationFeedback">
-                    <?php
-                    if (isset($errors['email'])) {
-                        echo $errors['email'];
-                    } else {
-                        echo "Champ obligatoire";
-                    }
-                    ?>
+
+                <div class="form-group col-md-6">
+                    <label for="email" class="form-label">Email : </label>
+                    <input type="email" class="form-control <?php if (isset($errors['email'])) echo 'is-invalid'; ?>" id="validationServer01" name="email" placeholder="adresse email" value="<?= isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>" required>
+                    <div class="invalid-feedback" id="emailValidationFeedback">
+                        <?php
+                        if (isset($errors['email'])) {
+                            echo $errors['email'];
+                        } else {
+                            echo "Champ obligatoire";
+                        }
+                        ?>
+                    </div>
                 </div>
-            </div>
-            </div>
 
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form2Example2">Mot de passe :</label>
-                <input type="password" class="form-control rounded mt-1 password-input <?php if (isset($errors['mot_de_passe'])) echo 'is-invalid'; ?>" name="mot_de_passe" placeholder="Votre mot de passe" aria-label="password" aria-describedby="password" id="password-input">
-                <div class="invalid-feedback" id="passwordValidationFeedback">Champ obligatoire</div>
-
-            </div>
-
-
-            <div class="col">
-                <a href="#!">Mot de passe perdu?</a>
-            </div>
-            </div>
-
-            <button class="button" type="submit" id="submitButton">Se connecter</button>
-
-            <div class="text-center">
-                <p>Pas encore membre? <a href="../controllers/controller-signup.php">Inscrivez-vous!</a></p>
+                <div class="form-group col-md-12">
+                    <label for="password-input" class="form-label">Mot de passe: </label>
+                    <div class="input-group d-flex">
+                        <input type="password" class="form-control rounded mt-1 password-input <?php if (isset($errors['mot_de_passe'])) echo 'is-invalid'; ?>" name="mot_de_passe" placeholder="Votre mot de passe" aria-label="password" aria-describedby="password" id="password-input">
+                        <div class="invalid-feedback" id="passwordValidationFeedback">Champ obligatoire</div>
+                    </div>
+                </div>
 
 
-            </div>
-        </form>
-
-    <?php } else { ?>
-        <p>ACCUEIL</p>
-    <?php } ?>
+                <div class="col">
+                    <a href="#">Mot de passe perdu?</a>
+                </div>
 
 
+                <div class="text-center">
+                    <button class="button" type="submit" id="submitButton">Se connecter</button>
+                </div>
+
+
+                <div class="text-center">
+                    <p>Pas encore membre? <a href="../controllers/controller-signup.php">Inscrivez-vous!</a></p>
+
+                </div>
+    </div>
+    </form>
+
+<?php } else { ?>
+    <p>ACCUEIL</p>
+<?php } ?>
+
+<script>
+    function toggleValidity(inputElement, feedbackElement) {
+        var inputValue = inputElement.value;
+
+        // Vérifier la validité de l'entrée
+        if (filter_var(inputValue, FILTER_VALIDATE_EMAIL)) {
+            inputElement.classList.remove("is-invalid");
+            inputElement.classList.add("is-valid");
+            feedbackElement.classList.remove("invalid-feedback");
+            feedbackElement.classList.add("valid-feedback");
+            feedbackElement.innerText = "Email valide";
+            feedbackElement.style.display = "block";
+        } else {
+            inputElement.classList.remove("is-valid");
+            inputElement.classList.add("is-invalid");
+            feedbackElement.classList.remove("valid-feedback");
+            feedbackElement.classList.add("invalid-feedback");
+            feedbackElement.innerText = "Email non valide";
+            feedbackElement.style.display = "block";
+        }
+    }
+
+    const emailInput = document.getElementById("validationServer01");
+    const emailFeedback = document.getElementById("emailValidationFeedback");
+
+    emailInput.addEventListener("input", function() {
+        toggleValidity(emailInput, emailFeedback);
+    });
+</script>
 
 </body>
 
