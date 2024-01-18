6b930a258343d83735a65f32deb274a11169974a
LN27100 <heloceinlovehotmail.fr>
2024-01-18 10:12:00 +0100

    empêcher accès home si pas connecté

--- controllers/controller-home.php
+++ controllers/controller-home.php
@@ -2,9 +2,18 @@
 require_once '../config.php';
 require_once '../models/Userprofil.php';
 
+// empêcher l'accès à la page home si l'utilisateur n'est pas connecté
+session_start();
+
+// Vérifiez si l'utilisateur est connecté
+if (!isset($_SESSION['pseudo'])) {
+    // Redirigez vers la page de connexion si la session n'est pas définie
+    header("Location: ../controllers/controller-signin.php");
+    exit();
+}
 
 
 
-
-
+// Inclure la vue home uniquement si l'utilisateur est connecté
 include_once '../views/view-home.php';
+?>
