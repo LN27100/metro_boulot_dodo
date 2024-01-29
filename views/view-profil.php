6b2fc56ef8ebb7deb7b2f0ebd9aabfa93a602c53
LN27100 <heloceinlovehotmail.fr>
2024-01-29 11:22:54 +0100

    modification du profil ok

--- views/view-profil.php
+++ views/view-profil.php
@@ -23,7 +23,7 @@
     <h1 class="titreAccueil">Votre profil</h1>
 
     <div class="container3">
-    <div class="profile-image-container">
+        <div class="profile-image-container">
             <img src="<?= $img ?>" alt="photo de profil" class="profile-image">
             <form method="post" action="../controllers/controller-profil.php" enctype="multipart/form-data" class="file-input-container">
                 <input type="file" name="profile_image" id="profile_image" required>
@@ -47,17 +47,33 @@
 
             <!-- Formulaire de modification de la description (masqué par défaut) -->
             <form method="post" action="../controllers/controller-profil.php" class="transparent-form" enctype="multipart/form-data" id="editDescriptionForm" style="display: none;">
-                <div class="profile-info">
-                    <p class="styleProfil">Modifier votre description:</p>
-                    <textarea id="user_describ" name="user_describ" rows="5" cols="33"><?= isset($_SESSION['user']['user_describ']) ? htmlspecialchars($_SESSION['user']['user_describ']) : "" ?></textarea>
-                    <input type="submit" name="save_description" value="Enregistrer" class="file-input-button">
-                    <button type="button" id="cancelEditBtn" class="file-input-button">Annuler</button>
-                </div>
-            </form>
+    <div class="profile-info">
+        <p class="styleProfil">Modifier votre description:</p>
+        <textarea id="user_describ" name="user_describ" rows="5" cols="33"><?= isset($_SESSION['user']['user_describ']) ? ($_SESSION['user']['user_describ']) : "" ?></textarea>
+
+        <p><span class="styleProfil"> Nom:</span></p>
+        <input type="text" name="user_name" placeholder="Nouveau nom" value="<?= $nom ?>">
+
+        <p><span class="styleProfil"> Prénom:</span></p>
+        <input type="text" name="user_firstname" placeholder="Nouveau prénom" value="<?= $prenom ?>">
+
+        <p><span class="styleProfil"> Pseudp:</span></p>
+        <input type="text" name="user_pseudo" placeholder="Nouveau pseudo" value="<?= $pseudo ?>">
+
+        <p><span class="styleProfil"> Email:</span></p>
+        <input type="text" name="user_email" placeholder="Nouveau email" value="<?= $email ?>">
+        
+        <p><span class="styleProfil"> Date de naissance:</span></p>
+        <input type="date" name="user_dateofbirth" placeholder="Nouvelle date de naissance" value="<?= $date_naissance ?>">
+
+        <input type="submit" name="save_modification" value="Enregistrer" class="file-input-button">
+        <button type="button" id="cancelEditBtn" class="file-input-button">Annuler</button>
+    </div>
+</form>
 
             <p><span class="styleProfil">Date de naissance: </span> <?= $date_naissance ?></p>
             <p><span class="styleProfil">Email: </span> <?= $email ?></p>
-            <p><span class="styleProfil">Entreprise:</span><?= $entreprise ?></p>
+            <p><span class="styleProfil">Entreprise: </span><?= $entreprise ?></p>
         </div>
     </div>
 
@@ -91,7 +107,7 @@
                 document.getElementById('editDescriptionForm').style.display = 'none';
             });
 
-           
+
         });
     </script>
 
