2ef88c2ac5bdc030f9d954f244d49c06880a3ab6
LN27100 <heloceinlovehotmail.fr>
2024-02-12 12:59:06 +0100

    ajout des pages erreurs 403 et 404

---
+++ notfound.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    </style>
+</head>
+
+<body>
+
+<img src="../assets/img/erreur404.php" alt="erreur 404">
+
+</body>
+</html>
